a690926d6e4d0c72a1d050b626661a9c23ba92b2
Fabio Coelho <fccoelho7@gmail.com>
2021-11-18 10:58:26 -0300

    send email notification

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const PORT = process.env.PORT || 3000;
 const Biscoint = require("biscoint-api-node").default;
+const axios = require("axios").default;
+
+const PORT = process.env.PORT || 3000;
 
 require("dotenv").config();
 
@@ -9,18 +11,39 @@ const bc = new Biscoint({
   apiSecret: process.env.API_SECRET,
 });
 
+function notify(template_id, template_params) {
+  return axios.post("https://api.emailjs.com/api/v1.0/email/send", {
+    service_id: "service_iplt3nr",
+    template_id,
+    template_params,
+    user_id: "user_PFZ8CRvPzDNFG1tQOTkNa",
+    accessToken: "3c20cef8d467b1d0185e36a53485d8f4",
+  });
+}
+
 express()
   .get("/balance", async (_, res) => {
     const data = await bc.balance();
     res.json(data);
   })
   .get("/buy", async (_, res) => {
-    const offer = await bc.offer({
-      amount: +process.env.AMOUNT,
-      isQuote: true,
-      op: "buy",
-    });
-    const confirmOrder = await bc.confirmOffer({ offerId: offer.offerId });
-    res.json(confirmOrder);
+    const amount = +process.env.AMOUNT;
+
+    try {
+      const offer = await bc.offer({
+        amount,
+        isQuote: true,
+        op: "buy",
+      });
+      const order = await bc.confirmOffer({ offerId: offer.offerId });
+
+      await notify("template_re6xyu7", { order });
+
+      res.json(order);
+    } catch (error) {
+      await notify("template_kt7p8dg", { amount, error });
+
+      res.json({ amount, error });
+    }
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
