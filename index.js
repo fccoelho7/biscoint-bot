e2f0bd936a3c390bd818d08c592ba379b7037dcd
Fabio Coelho <fccoelho7@gmail.com>
2021-11-19 10:17:56 -0300

    remove dotenv import

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { bc } = require("./services/bc");
 
 const PORT = process.env.PORT || 3000;
 
-require("dotenv").config();
-
 express()
   .get("/", async (_, res) => {
     const data = await bc.balance();
